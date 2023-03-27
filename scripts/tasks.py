f4913356c81d5a31f6c93203d6954da7c2dcdac0
PedroFlorencioNeto <pedroflorencio@alu.ufc.br>
2023-03-27 13:43:35 -0300

    adicionando arquitetura delta lake e finalizando task extract

--- scripts/tasks.py
+++ scripts/tasks.py
@@ -1,14 +1,23 @@
 import boto3
+import numpy as np
 import pandas as pd
+from io import StringIO
 from logging import info
 from prefect import task
 from configparser import ConfigParser
 
+# Pipeline de Dados EtLT - Arquitetura Delta Lake
+
+# (E)xtração de Dados de Bucket S3;
+# (t)ransformação e agregação dos anos em Pandas DataFrame;
+# (L)oad de dados no banco de dados Postgres e na pasta "Silver";
+# (T)ransformação de dados para uso a nível de negócio e exportação para a pasta "Gold".
+
 #@task(name="Extracao de dados", description="Extrai dados contidos em um bucket S3")
-def extract_data(bucket_name: str, folder_name: str) -> str:
+def extract_data(bucket_name: str, folder_name: str) -> pd.DataFrame:
     '''
     Função que realiza o download dos arquivos (formato xls ou xlsx) de sinistros de trânsito 
-    contidos em um bucket S3 da AWS e retorna um texto em formato CSV.
+    contidos em um bucket S3 da AWS e retorna um pandas DataFrame.
 
     A pasta no bucket deve conter apenas dados anuais em formato CSV seguindo as regras:
     - Sinistros_YYYY.xls
@@ -25,24 +34,34 @@ def extract_data(bucket_name: str, folder_name: str) -> str:
     # Acessando credenciais da AWS
     config = ConfigParser()
     config.read('pipeline.conf')
-    ACCESS_KEY = config['aws_boto_credentials']['access_key']
-    SECRET_KEY = config['aws_boto_credentials']['secret_key']
-    
+    ACCESS_KEY = config.get('aws_boto_credentials', 'access_key')
+    SECRET_KEY = config.get('aws_boto_credentials', 'secret_key')
+
     # Acessando os dados contidos no bucket especificado
-    s3_resource = boto3.resource('s3', aws_access_key_id=ACCESS_KEY, aws_secret_key_id=SECRET_KEY)
-    Bucket = s3_resource.Bucket(bucket_name)
-  
+    s3_client = boto3.client("s3", aws_access_key_id = ACCESS_KEY, aws_secret_access_key = SECRET_KEY)
+    s3_resource = boto3.resource('s3', aws_access_key_id = ACCESS_KEY, aws_secret_access_key = SECRET_KEY)
+    Bucket= s3_resource.Bucket(bucket_name)
+    df_temp = pd.DataFrame()
+
     for object in Bucket.objects.filter(Prefix=folder_name):
-        print(object.key)
+        if object.key != folder_name:
+            response = s3_client.get_object(Bucket=bucket_name, Key=object.key)
+            df = pd.read_csv(StringIO(response['Body'].read().decode('ISO-8859-1')),sep=';')
+            df = df.sum()
+            consolidado_por_ano = {'Automovel':df['AUTOMOVEL'],
+                                    'Motocicleta':df['MOTOCICLETA'],
+                                    'Bicicleta':df['BICICLETA'],
+                                    'Outros': (df['CAMINHAO']+df['MICROONIBUS']+df['CICLOMOTOR']+df['TREM']),
+                                    'Mortos':df['MORTOS'],
+                                    'Feridos':df['FERIDOS'],
+                                    'Ilesos':df['ILESOS']}
+            df_temp = df_temp.append(consolidado_por_ano, ignore_index=True)
+    df_temp['Ano'] = np.arange(2015,2020,1)
+    df = df_temp
+
     info('Os dados foram baixados corretamente!')
 
-    return "Pedro"
+    return df
 
-extract_data("diobs-frente-verso-ciclomobilidade", "Sinistros/")
-
-@task
-def transform_data():
-
-@task
-def load_data(dataFrame: pd.DataFrame) ->
-
+bucket = "diobs-frente-verso-ciclomobilidade"
+folder = "Sinistros/"
